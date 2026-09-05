17f4400c1d8cd27cd5758e8bfe848742ffb74c52
FanMclaine <mclainerudyanto@gmail.com>
2026-09-05 18:14:25 +0800

    add new blog entry

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,23 +2,23 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/me.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "Mclaine Rudyanto",
-	bio: "Tinkerer and dabbler of many things.",
-	typewriter: {
-		enable: true, // 启用个人简介打字机效果
-		speed: 80, // 打字速度（毫秒）
-	},
-	links: [
-		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/Fanmclaine",
-		},
-		{
-			name: "Discord",
-			icon: "fa7-brands:discord",
-			url: "https://discord.gg/mclnoot",
-		},
-	],
+  avatar: "assets/images/me.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+  name: "Mclaine Rudyanto",
+  bio: "Self-sabotaging my responsibilities through nonsense like this. 我不知道该怎么办了。",
+  typewriter: {
+    enable: true, // 启用个人简介打字机效果
+    speed: 80, // 打字速度（毫秒）
+  },
+  links: [
+    {
+      name: "GitHub",
+      icon: "fa7-brands:github",
+      url: "https://github.com/Fanmclaine",
+    },
+    {
+      name: "Discord",
+      icon: "fa7-brands:discord",
+      url: "https://discordapp.com/users/697771701127217173",
+    },
+  ],
 };
